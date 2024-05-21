2fe311dcdd74e6a972544384d8e609a445931f42
YanpieroCutipa <yanpiero_cutipa@usmp.pe>
2024-05-20 21:28:47 -0500

    Agregando First Integracion API

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using replicaPROGRAMA.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 using Microsoft.OpenApi.Models;
 using replicaPROGRAMA.Service;
+using replicaPROGRAMA.Integration.jsonplaceholder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +29,8 @@ builder.Services.AddControllersWithViews();
 //Registro mi logica customizada y reuzable
 builder.Services.AddScoped<ProductoService, ProductoService>();
 
+//REGISTRAR el SERVICIO
+builder.Services.AddScoped<JsonplaceholderApiIntegration, JsonplaceholderApiIntegration>();
 
 builder.Services.AddSession(options =>
 {
